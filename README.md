d2a9d577a4735a9101ba0a19bbf60876287cf8fc
0xori <kadosh.ori@gmail.com>
2016-05-22 16:15:16 +0300

    package.json edit

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 "# open-walkthrough" 
+
+this is a client side website walkthrough executor engine.
+
+installation:
+npm i open-walkthrough
+
+usage:
+
+var walkThoughExecutor = require("./src/walkThoughExecutor");
+var actionTypes = require("./src/actionTypes");
+
+var action1 = {
+    selector: '#yo1',
+    type: actionTypes.ACTION_POPOVER,
+    data: {
+        title: "Click Here",
+        description: "Get in Shape"
+    }
+};
+var action2 = {
+    selector: '#yo2',
+    type: actionTypes.ACTION_POPOVER,
+    data: {
+        title: "Click Here",
+        description: "Get in Shape"
+    }
+};
+var action3 = {
+    selector: '#yo3',
+    type: actionTypes.ACTION_POPOVER,
+    data: {
+        title: "Click Here",
+        description: "Get in Shape"
+    }
+};
+var action4 = {
+    selector: '#yo4',
+    type: actionTypes.ACTION_POPOVER,
+    data: {
+        title: "Click Here",
+        description: "Get in Shape"
+    }
+};
+
+var actions = [action1,action2,action3,action4];
+
+walkThoughExecutor.setActions(actions);
+walkThoughExecutor.run();
+
+
--- main.js
+++ main.js
@@ -3,41 +3,41 @@ var walkThoughExecutor = require("./src/walkThoughExecutor");
 var actionTypes = require("./src/actionTypes");
 
 var action1 = {
-            selector: '#yo1',
-            type: actionTypes.ACTION_POPOVER,
-            data: {
-                title: "Click Here",
-                description: "Get in Shape"
-            }
-        };
+    selector: '#yo1',
+    type: actionTypes.ACTION_POPOVER,
+    data: {
+        title: "Click Here",
+        description: "Get in Shape"
+    }
+};
 var action2 = {
-     selector: '#yo2',
-            type: actionTypes.ACTION_POPOVER,
-            data: {
-                title: "Click Here",
-                description: "Get in Shape"
-            }
-        };
-        var action3 = {
-            selector: '#yo3',
-            type: actionTypes.ACTION_POPOVER,
-            data: {
-                title: "Click Here",
-                description: "Get in Shape"
-            }
-        };
-        var action4 = {
-            selector: '#yo4',
-            type: actionTypes.ACTION_POPOVER,
-            data: {
-                title: "Click Here",
-                description: "Get in Shape"
-            }
-        };
+    selector: '#yo2',
+    type: actionTypes.ACTION_POPOVER,
+    data: {
+        title: "Click Here",
+        description: "Get in Shape"
+    }
+};
+var action3 = {
+    selector: '#yo3',
+    type: actionTypes.ACTION_POPOVER,
+    data: {
+        title: "Click Here",
+        description: "Get in Shape"
+    }
+};
+var action4 = {
+    selector: '#yo4',
+    type: actionTypes.ACTION_POPOVER,
+    data: {
+        title: "Click Here",
+        description: "Get in Shape"
+    }
+};
 
-var actions = [action1,action2,action3,action4];
+var actions = [action1, action2, action3, action4];
 
-$(document).ready(function(){
+$(document).ready(function () {
     walkThoughExecutor.setActions(actions);
     walkThoughExecutor.run();
 });
